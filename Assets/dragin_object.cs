0dafc86d9530f941988755a586a91052ce8c8301
Catharactus <A.azoulay@rubika-edu.com>
2024-04-06 21:24:25 -0400

    fresh start

---
+++ Assets/dragin_object.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragin_object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 1; i < 10; i++) 
+        {
+            Debug.Log(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
