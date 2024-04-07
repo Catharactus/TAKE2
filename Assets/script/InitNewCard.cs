febe4222bf0c731ca6c4214fc503604c5014c8ee
Catharactus <A.azoulay@rubika-edu.com>
2024-04-07 15:00:56 -0400

    commit test

--- Assets/script/InitNewCard.cs
+++ Assets/script/InitNewCard.cs
@@ -1,38 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Presets;
 using UnityEngine;
 
 public class InitNewCard : MonoBehaviour
 {
-    public void InitCard(string Tag)
+    public void InitCard(string CardTag)
     {
-        Debug.Log(FindCorrectInitPreset(Tag));
+        //Debug.Log(FindCorrectInitPreset(Tag));
+        int preset = FindCorrectInitPreset(CardTag);
+
+        switch(preset)
+        {
+            default:
+                Debug.Log("cardFailled");
+                break;
+
+            case 1:
+                InitCardAsWork();
+                break;
+
+
+            case 2:
+                InitCardAsMood();
+                break;
+
+            case 3:
+                InitCardAsLeisure();
+                break;
+        }
+
     }
 
-    int FindCorrectInitPreset(string Tag)
+    int FindCorrectInitPreset(string CardTag)
     {
 
         int preset = 0;
 
-        if(Tag == "work")
+        if(CardTag == "work")
         {
             preset = 1;
             return preset;
         }
 
-        if(Tag == "mood")
+        if(CardTag == "mood")
         {
             preset = 2;
             return preset;
         }
 
-        if (Tag == "leisure")
+        if (CardTag == "leisure")
         { 
             preset = 3;
             return preset;
         }
 
         return preset;
+    }
 
+    private void InitCardAsWork()
+    {
+        return;
+    }
+
+    private void InitCardAsMood()
+    {
+        return;
+    }
+
+    private void InitCardAsLeisure()
+    {
+        return;
     }
 }
