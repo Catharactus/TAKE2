6dce3d302a7f268f6686dc346bd8f1d56a64df63
Catharactus <A.azoulay@rubika-edu.com>
2024-04-07 03:50:06 -0400

    fgzs

--- Assets/dragin_object.cs
+++ Assets/dragin_object.cs
@@ -4,19 +4,73 @@ using UnityEngine;
 
 public class dragin_object : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    Vector3 mousePositionOffset;
 
-        for (int i = 1; i < 10; i++) 
-        {
-            Debug.Log(i);
-        }
+    bool placeIsCorrect;
+
+    Vector3 blockInitialPosition;
+
+    public float returnTime;
+
+    private void Start()
+    {
+        InitBlok();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitBlok()
     {
-        
+        blockInitialPosition = transform.position;
+    }
+    
+    private Vector3 GetMouseWorldPosition()
+    {
+        // Capture the mouse position and convert it to world coordinates
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDown()
+    {
+        // Calculate the offset between the object's position and the mouse position
+        mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        // Update the object's position based on the mouse movement
+        transform.position = GetMouseWorldPosition() + mousePositionOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        //check the closest tile to mouse position 
+
+        //check if the tile is correct for the block
+
+        if(placeIsCorrect)
+        {
+            //place tile
+        }
+        else
+        {
+            //return tile to begin place 
+            StartCoroutine(ReturnToInitialPos());
+        }
+
+    }
+
+    private IEnumerator ReturnToInitialPos()
+    {
+
+        float elaspeTime = 0f;
+        Vector3 currentBlockPos = transform.position;
+
+        while (elaspeTime < returnTime)
+        {
+            transform.position = Vector3.Lerp(currentBlockPos, blockInitialPosition, elaspeTime / returnTime);
+            elaspeTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = blockInitialPosition;
     }
 }
